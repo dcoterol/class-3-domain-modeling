24af2c09295f311f7ea155accebf36a7681db735
Dany Cotero <dany.cotero@kellogg.northwestern.edu>
2023-04-13 01:25:10 +0000

    finished school.sql practice

--- school/school.sql
+++ school/school.sql
@@ -16,4 +16,34 @@ CREATE TABLE students (
 
 -- Create the rest of the tables
 
+CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  bio TEXT
+);
 
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  description TEXT
+);
+
+CREATE TABLE sections (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time TEXT,
+  course_id INTEGER,
+  teacher_id INTEGER
+);
+
+-- Design Design the model that allows students to sign up for classes – let's call it Enrollment. 
+-- Create the enrollments
+
+CREATE TABLE enrollments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  student_id INTEGER,
+  -- I don't need to add course_id because section and course are already connected
+  section_id INTEGER,
+  status TEXT
+);
